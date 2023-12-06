c615eda8cf2d986244026c6e921ac23344b11d90
Siyaram Sharan Prasad <siyaram3572@gmail.com>
2023-12-06 15:30:37 +0530

    After adding @ComponentScan annotation with different paths

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo.controllers"})
+//@ComponentScan("com.example.demo.src.main.resources.templates.webapp.WEB-INF.pages")
+//@ComponentScan("C:/Users\Siyaram\Downloads\demo\demo\src\main\resources\templates\webapp\WEB-INF\pages")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/example/demo/controllers/MainController.java
+++ demo/src/main/java/com/example/demo/controllers/MainController.java
@@ -2,8 +2,10 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
     @GetMapping("/hello")
     public String greet() {
